ba2aeb466d0f9a652c1a65f0464005efbc822645
woook <woook@users.noreply.github.com>
2021-04-13 16:32:38 +0100

    dev version 1.1.9

--- Readme.md
+++ Readme.md
@@ -1,1 +1,29 @@
-# eggd_vcf_handler_for_uranus
+# eggd vcf handler for uranus
+
+
+## What does this app do?
+
+vcf processing
+
+## What are typical use cases for this app?
+
+* bcftools (v1.12)
+* bedtools (v2.30.0)
+* bgzip (v1.12)
+* samtools (v1.12)
+* tabix (v1.12)
+
+VEP (v103.1) Docker images
+
+Has access to the Internet
+
+## What data are required for this app to run?
+
+
+## What does this app output?
+
+
+## How does this app work?
+
+
+## Examples
---
+++ src/code.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+
+# The following line causes bash to exit at any point if there is any error
+# and to output each line as it is executed -- useful for debugging
+set -e -x -o pipefail
+
+mark-section "downloading inputs"
+dx-download-all-inputs --parallel
+
+mark-section "filtering variants"
+# retain variants that are within ROIs (bed file), have at least one allele >0.03 AF, have DP >99
+bedtools intersect -header -a $vcf_path -b $bed_path | bcftools view -i "FORMAT/AF[*]>0.03" - | bcftools view -i "DP>99" - -o ~/$vcf_prefix.filter.vcf
+
+mark-section "annotating variants"
+# vep needs permissions to write to /home/dnanexus
+chmod a+rwx /home/dnanexus
+# extract annotation tarball to /home/dnanexus
+tar xf $vep_annotation_path -C /home/dnanexus
+# place fasta and indexes for VEP in the annotation folder
+mv $vep_fasta_path /home/dnanexus/homo_sapiens_refseq/103_GRCh38/
+mv $vep_fai_path /home/dnanexus/homo_sapiens_refseq/103_GRCh38/
+mv $vep_gzi_path /home/dnanexus/homo_sapiens_refseq/103_GRCh38/
+# load vep docker
+docker load -i $vep_docker_path
+# run vep
+docker run -v /home/dnanexus:/opt/vep/.vep ensemblorg/ensembl-vep:release_103.1 ./vep -i /opt/vep/.vep/$vcf_prefix.filter.vcf -o /opt/vep/.vep/$vcf_prefix.annotate.vcf --vcf --cache --refseq --exclude_predicted --symbol --hgvs --af_gnomad --check_existing --variant_class --numbers --fields "SYMBOL,VARIANT_CLASS,Consequence,EXON,HGVSc,HGVSp,gnomAD_AF,Existing_variation" --no_stats --transcript_filter "stable_id match NM_002074 or stable_id match NM_000760 or stable_id match NM_005373 or stable_id match NM_002227 or stable_id match NM_002524 or stable_id match NM_022552 or stable_id match NM_012433 or stable_id match NM_005896 or stable_id match NM_002468 or stable_id match NM_032638 or stable_id match NM_000222 or stable_id match NM_001127208 or stable_id match NM_033632 or stable_id match NM_002520 or stable_id match NM_016222 or stable_id match NM_006060 or stable_id match NM_181500 or stable_id match NM_004333 or stable_id match NM_004456 or stable_id match NM_170606 or stable_id match NM_006265 or stable_id match NM_004972 or stable_id match NM_016734 or stable_id match NM_017617 or stable_id match NM_000314 or stable_id match NM_005343 or stable_id match NM_024426 or stable_id match NM_001165 or stable_id match NM_000051 or stable_id match NM_001197104 or stable_id match NM_005188 or stable_id match NM_001987 or stable_id match NM_018638 or stable_id match NM_033360 or stable_id match NM_001136023 or stable_id match NM_005475 or stable_id match NM_002834 or stable_id match NM_004119 or stable_id match NM_002168 or stable_id match NM_004380 or stable_id match NM_000546 or stable_id match NM_001042492 or stable_id match NM_012448 or stable_id match NM_139276 or stable_id match NM_003620 or stable_id match NM_001195427 or stable_id match NM_015559 or stable_id match NM_004343 or stable_id match NM_004364 or stable_id match NM_015338 or stable_id match NM_080425 or stable_id match NM_001754 or stable_id match NM_006758 or stable_id match NM_007194 or stable_id match NM_001429 or stable_id match NM_005089 or stable_id match NM_001123385 or stable_id match NM_002049 or stable_id match NM_001042750 or stable_id match NM_001184772 or stable_id match NM_001015877"
+# run vep filtering
+docker run -v /home/dnanexus:/opt/vep/.vep ensemblorg/ensembl-vep:release_103.1 ./filter_vep -i /opt/vep/.vep/$vcf_prefix.annotate.vcf -o /opt/vep/.vep/$vcf_prefix.filter.annotate.vcf --only_matched --filter "(gnomAD_AF < 0.05 or not gnomAD_AF) and (Consequence != synonymous_variant)"
+
+mark-section "uploading output"
+mkdir -p ~/out/filtered_annotated_vcf
+mv ~/$vcf_prefix.filter.annotate.vcf ~/out/filtered_annotated_vcf/
+dx-upload-all-outputs --parallel
+
+mark-success
